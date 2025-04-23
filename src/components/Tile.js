5533d9cbed4d8e76fc9f4c7f26f48f1551375471
Spudulene <147779189+Spudulene@users.noreply.github.com>
2025-04-23 16:10:51 -0400

    better tile comment

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -8,9 +8,10 @@ const Tile = ({ value, onClick, backgroundImage, gridSize }) => {
   const tileStyle = {
     // set the background image for a tile as the full image if it is not empty
     backgroundImage: backgroundImage && !isEmpty ? `url(${backgroundImage})` : undefined,
-    // blow upd the image by the size
+    // blow up the image so that we are able to display a small 
+    // portion of the whole image on each tile
     backgroundSize: `${gridSize * 100}%`,
-    // set the visible portion of the image based on where its tile is
+    /// set the visible portion of the image based on what the tile value is (where the tile should be)
     backgroundPosition: backgroundImage && !isEmpty 
       ? `${(value - 1) % gridSize * (100 / (gridSize - 1))}% ${Math.floor((value - 1) / gridSize) * (100 / (gridSize - 1))}%`
       : undefined,
